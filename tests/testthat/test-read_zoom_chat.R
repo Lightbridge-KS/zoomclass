3848eb2eda93f80d54b4e140e4dddb25c2e3fe52
Lightbridge-AI <67057499+Lightbridge-AI@users.noreply.github.com>
2021-10-14 14:45:03 +0700

    Add more tests

--- tests/testthat/test-read_zoom_chat.R
+++ tests/testthat/test-read_zoom_chat.R
@@ -2,22 +2,24 @@ test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
 
-# test_that("read_zoom_chat is working",{
-#   # Full & Abbreviated
-#   chat_df_full <- readzoom::read_zoom_chat(("tests/testdata/Zoom-chat-ex.txt"))
-#   chat_df_abbr <- readzoom::read_zoom_chat(("tests/testdata/Zoom-chat-ex-abbr.txt"))
-#
-#   expect_s3_class(chat_df_full, "tbl_df")
-#   expect_s3_class(chat_df_abbr, "tbl_df")
-#
-# })
 
-#system.file("testdata","Zoom-chat-ex.txt",package="readzoom") %>% readzoom::read_zoom_chat()
+# Test path to "testdata" folder ------------------------------------------
+
+
+test_that("`system.file path works`",{
+  path <- system.file("testdata", package="readzoom")
+  expect_false(path == "")
+})
+
+
+# read_zoom_chat ----------------------------------------------------------
 
 
 test_that("read_zoom_chat is working",{
   # Full & Abbreviated
   chat_df_full <- readzoom::read_zoom_chat(system.file("testdata","Zoom-chat-ex.txt",package="readzoom"))
-  expect_s3_class(chat_df_full, "tbl_df")
+  chat_df_abbr <- readzoom::read_zoom_chat(system.file("testdata","Zoom-chat-ex-abbr.txt",package="readzoom"))
+   expect_s3_class(chat_df_full, "tbl_df")
+   expect_s3_class(chat_df_abbr, "tbl_df")
 
 })
