b3abba5950b9e8efe6151bf8bf00d581b7fe76c1
Lightbridge-AI <67057499+Lightbridge-AI@users.noreply.github.com>
2021-11-21 22:10:10 +0700

    global vars stuff

--- R/globals.R
+++ R/globals.R
@@ -1,4 +1,7 @@
 # Put this in  R/globals.R
 utils::globalVariables(c("Time",".",
-                         "Meeting_ID", "Name (Original Name)"
+                         "Meeting_ID", "Name (Original Name)",
+                         "Class_End", "Class_Start", "Email", "Join_Time", "Leave_Time",
+                         "Duration_Minutes", "First_Join_Time", "Multi_Device", "Name", "Name_Original", "Session"
+
 ))
