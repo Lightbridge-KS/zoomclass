8a4d0c92d34fa2a0ae7169bbd54e647a22cd44da
Lightbridge-AI <67057499+Lightbridge-AI@users.noreply.github.com>
2021-11-21 22:12:47 +0700

    add test data

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -24,3 +24,14 @@ pp_cal_cleaned <- read_participants(paths.testdata$pp_cal)
 pp_mm2_cleaned <- read_participants(paths.testdata$pp_mm2)
 
 
+
+# Class Time --------------------------------------------------------------
+
+
+class_tm1 <- data.frame(start =  lubridate::dmy_hms("10/06/2021 10:00:00 AM"),
+                        end = lubridate::dmy_hms("10/06/2021 11:00:00 AM"),
+                        join = lubridate::dmy_hms("10/06/2021 09:00:00 AM"),
+                        leave = lubridate::dmy_hms("10/06/2021 10:30:00 AM"))
+
+
+
