b4cd0f3cb3ad544a6e7583e2b5a90738ec7c3624
Lightbridge-AI <67057499+Lightbridge-AI@users.noreply.github.com>
2021-11-23 15:12:00 +0700

    globals stuff

--- R/globals.R
+++ R/globals.R
@@ -2,6 +2,7 @@
 utils::globalVariables(c("Time",".",
                          "Meeting_ID", "Name (Original Name)",
                          "Class_End", "Class_Start", "Email", "Join_Time", "Leave_Time",
-                         "Duration_Minutes", "First_Join_Time", "Multi_Device", "Name", "Name_Original", "Session"
+                         "Duration_Minutes", "First_Join_Time", "Multi_Device", "Name",
+                         "Name_Original", "Session", "ID"
 
 ))
