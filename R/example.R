fa1788c7fe725a4365e4a26ee1a8c3856638e860
Lightbridge-AI <67057499+Lightbridge-AI@users.noreply.github.com>
2021-11-29 23:25:26 +0700

    add `zoomclass_example()`

---
+++ R/example.R
@@ -0,0 +1,21 @@
+#' Get path to zoomclass example
+#'
+#' zoomclass comes bundled with a number of sample files in its `inst/extdata`
+#' directory. This function make them easy to access
+#'
+#' @param file Name of file. If `NULL`, the example files will be listed.
+#' @export
+#' @examples
+#' library(zoomclass)
+#' zoomclass_example()
+#' zoomclass_example("participants_heroes.csv")
+zoomclass_example <- function(file = NULL) {
+
+  if (is.null(file)) {
+    dir(system.file("extdata", package = "zoomclass"))
+
+  } else {
+    system.file("extdata", file, package = "zoomclass", mustWork = TRUE)
+  }
+
+}
