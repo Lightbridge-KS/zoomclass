0bbf8a9984f3ad7b724f9fed2916912f33c16f17
Kittipos <ki11ip0.s.a.s@gmail.com>
2021-12-28 11:14:31 +0700

    Update `heroes_students` data to include student not in zoom class and zoom participants who's not a student

--- R/data-example.R
+++ R/data-example.R
@@ -1,11 +1,11 @@
 #' Prices of 50,000 round cut diamonds.
 #'
-#' A simulated dataset containing names and student's ID of heroes who enrolled in a semester.
+#' A simulated data set containing names and student's ID of heroes who enrolled in a semester.
 #'
 #' @format A data frame with 72 rows and 3 variables:
 #' \describe{
 #'   \item{ID}{Student's ID}
 #'   \item{Name}{Student's Name}
-#'   \item{Enrolled}{Logical, If `TRUE` means student is enrolled, otherwise `FALSE`.}
+#'   \item{Comment}{Character, Provide descriptions about students.}
 #' }
 "heroes_students"
