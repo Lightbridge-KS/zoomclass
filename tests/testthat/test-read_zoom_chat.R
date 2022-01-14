a88eab50f9b4e933c27176a05f2bff4fb8e37962
Kittipos <ki11ip0.s.a.s@gmail.com>
2022-01-14 20:18:30 +0700

    Update test to check "zoom_chat" class

--- tests/testthat/test-read_zoom_chat.R
+++ tests/testthat/test-read_zoom_chat.R
@@ -19,7 +19,9 @@ test_that("read_zoom_chat is working",{
   # Full & Abbreviated
   chat_df_full <- zoomclass::read_zoom_chat(path_testdata("chat/Zoom-chat-ex.txt"))
   chat_df_abbr <- zoomclass::read_zoom_chat(path_testdata("chat/Zoom-chat-ex-abbr.txt"))
-   expect_s3_class(chat_df_full, "tbl_df")
-   expect_s3_class(chat_df_abbr, "tbl_df")
+  expect_s3_class(chat_df_full, "zoom_chat")
+  expect_s3_class(chat_df_abbr, "zoom_chat")
 
 })
+
+
